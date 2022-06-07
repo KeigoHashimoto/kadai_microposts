91610de7d5aa25f0e901530912326b55f74da77e
Keigo HAshimoto <kei.work.info@gmail.com>
2022-06-07 22:44:13 +0000

    top page

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,19 @@
+<header class="mb-4">
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        <a class="navbar-brand" href="/">Microposts</a>
+        
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbar">
+            <span class="navbar-togglar-icon"></span>
+        </button>
+        
+        <div class="collapse navbar-collapse" id="navbar">
+            <ul class="navbar-nav mr-auto"></ul>
+            <ul class="navbar-nav">
+                {{--ユーザー登録ページへのリンク--}}
+                <li class="nav-item"><a href="#" class="nav-link">Signup</a></li>
+                {{--ログインページへのリンク--}}
+                <li class="nav-item"><a href="#" class="nav-link">Signup</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
